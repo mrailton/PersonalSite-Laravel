751edf8d5d3d6c0c0ae9c33310f98f357f1f0747
Mark Railton <mark.railton@outlook.ie>
2024-11-25 17:37:39 +0000

    Add new tidy method

--- app/View/Components/GuestHeader.php
+++ app/View/Components/GuestHeader.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\View\Components;
 
 use Closure;
@@ -8,17 +10,11 @@ use Illuminate\View\Component;
 
 class GuestHeader extends Component
 {
-    /**
-     * Create a new component instance.
-     */
     public function __construct()
     {
         //
     }
 
-    /**
-     * Get the view / contents that represent the component.
-     */
     public function render(): View|Closure|string
     {
         return view('components.guest-header');
