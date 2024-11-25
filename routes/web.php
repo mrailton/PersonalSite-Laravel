3f6ee85b8d13e2b213a1eccb8111b11f22c01325
Mark Railton <mark.railton@outlook.ie>
2024-11-25 19:41:27 +0000

    Remove unused template add alpine to guest layout

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', IndexController::class)->name('index');
 Route::get('/blog', ListArticlesController::class)->name('articles.list');
 Route::get('/blog/{article:slug}', ShowArticleController::class)->name('articles.show');
+
