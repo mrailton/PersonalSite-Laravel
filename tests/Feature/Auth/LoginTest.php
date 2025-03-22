25f12cec2a13ae83fffb6fe1d524fdf4d4b5dc58
Mark Railton <mark.railton@outlook.ie>
2025-03-22 01:13:55 +0000

    Make a start with auth tests

---
+++ tests/Feature/Auth/LoginTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature\Auth;
+
+use App\Models\User;
+use JsonException;
+use PHPUnit\Framework\Attributes\Test;
+use Tests\TestCase;
+
+class LoginTest extends TestCase
+{
+    #[Test]
+    public function login_page_renders(): void
+    {
+        $this->get(route('auth.login'))
+            ->assertOk()
+            ->assertSee('Sign in to your account')
+            ->assertViewIs('auth.login');
+    }
+
+    /**
+     * @throws JsonException
+     */
+    #[Test]
+    public function user_can_login(): void
+    {
+        $user = User::factory()->create();
+
+        $response = $this->post(route('auth.login'), [
+            'email' => $user->email,
+            'password' => 'password',
+        ]);
+
+        $response->assertRedirect(route('admin.dashboard'))
+            ->assertSessionHasNoErrors();
+    }
+}
